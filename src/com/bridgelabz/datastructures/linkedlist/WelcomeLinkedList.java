ef1adca75af21a229708d312209f07ebbb292246
navyas211@gmail.com <navyas211@gmail.com>
2021-10-08 22:06:13 +0530

    [NAVYA]ADDED: Welcome to DataStructures LinkedList Programs

---
+++ src/com/bridgelabz/datastructures/linkedlist/WelcomeLinkedList.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.datastructures.linkedlist;
+
+public class WelcomeLinkedList {   
+	public static void main(String[] args) {
+		System.out.println("Welcome To Data Structures In LinkedList Programs ");
+	}
+
+}
